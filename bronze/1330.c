9059cf4dd46ecb958b546e7bf99542c3833c993e
kimseongbeen <allcbin2@gmail.com>
2026-03-24 16:08:27 +0900

    1330.c

---
+++ bronze/1330.c
@@ -0,0 +1,26 @@
+﻿
+//두 수 비교하기//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if(a>b)
+    {
+        printf(">");
+    }
+    else if(a<b)
+    {
+        printf("<");
+    }
+    else
+    {
+        printf("==");
+    }
+
+    return 0;
+
+}
