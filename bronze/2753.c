0711bd9ac7f1a5173f14b9e524f16da846eeb6f7
kimseongbeen <allcbin2@gmail.com>
2026-03-24 16:17:24 +0900

    2753.c

---
+++ bronze/2753.c
@@ -0,0 +1,27 @@
+
+//윤년//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    if(n%400==0)
+    {
+        printf("%d", 1);
+    }
+    else if(n%4==0&&n%100!=0)
+    {
+        printf("%d", 1);
+    }
+    else
+    {
+        printf("%d", 0);
+    }
+
+    return 0;
+
+}
