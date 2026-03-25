6c2253af9b2df4f3ff2dbe3044850ead7f18484d
kimseongbeen <allcbin2@gmail.com>
2026-03-25 11:01:49 +0900

    11382.c

---
+++ bronze/11382.c
@@ -0,0 +1,15 @@
+
+//꼬마 정민//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    long long int a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld", a+b+c);
+
+    return 0;
+    
+}
