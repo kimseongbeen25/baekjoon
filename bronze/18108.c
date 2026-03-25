c8ad23a6100bca1c4bcdeeb5090c06c224791e03
kimseongbeen <allcbin2@gmail.com>
2026-03-25 10:52:19 +0900

    18108.c

---
+++ bronze/18108.c
@@ -0,0 +1,17 @@
+
+//1998년생인 내가 태국에서는 2541년생?!//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    int year;
+    scanf("%d", &year);
+
+    year-=543;
+    printf("%d", year);
+
+    return 0;
+    
+}
