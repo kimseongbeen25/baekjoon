480ceaf8ca11b151204d8e9437e9194bf55ec72e
kimseongbeen <allcbin2@gmail.com>
2026-03-28 15:42:13 +0900

    25314.c

---
+++ bronze/25314.c
@@ -0,0 +1,22 @@
+
+//코딩은 체육과목 입니다//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+    int a;
+    scanf("%d", &n);
+    a = n/4;
+    for(int i=1; i<=a; i++)
+    {
+        printf("long ");
+    }
+
+    printf("int");
+
+    return 0;
+    
+}
