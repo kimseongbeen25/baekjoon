5d345c9094a77a78d9abe7ffeceb45d458767f8c
kimseongbeen <allcbin2@gmail.com>
2026-03-28 15:31:10 +0900

    10950.c

---
+++ bronze/10950.c
@@ -0,0 +1,22 @@
+
+//A+B - 3//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+    int a,b;
+
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+
+}
